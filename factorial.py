9f639b3688e86c6d3d17bea7ebeb2a21107e4b70
ThisIsanAlt <50222844+ThisIsanAlt@users.noreply.github.com>
2020-07-10 16:41:09 -0400

    renames factorial() to recursivefactorial()

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
-def factorial(n : int):
+def recursivefactorial(n : int):
   if n == 1: return n
-  else: return n*factorial(n-1)
+  else: return n*recursivefactorial(n-1)
 
 while True:
-  print(factorial(int(input('What number would you like to factorialize?'))))
+  print(recursivefactorial(int(input('What number would you like to factorialize?'))))
